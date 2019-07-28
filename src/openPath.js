0221521f00bae2acd07f4b3225878eb0068a89cd
Rodrigo Fernandes da Costa <fcostarodrigo@yahoo.com>
2019-07-28 22:22:52 +0100

    Refactor

--- src/openPath.js
+++ src/openPath.js
@@ -5,11 +5,14 @@ const folders = require("./folders");
 const mkdir = util.promisify(fs.mkdir);
 
 const openPath = async (pathToOpen, fileInPath) => {
-  const ignoreExistError = error =>
-    error.code === "EEXIST" ? Promise.resolve() : Promise.reject(error);
-
   for (const folder of folders(pathToOpen, fileInPath)) {
-    await mkdir(folder).catch(ignoreExistError);
+    try {
+      await mkdir(folder);
+    } catch (error) {
+      if (error.code !== "EEXIST") {
+        throw error;
+      }
+    }
   }
 };
 
